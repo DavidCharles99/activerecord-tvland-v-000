6138090a912d8730badc981ceadad86e5f6982df
David Charles <ratlover03@gmail.com>
2019-06-24 22:47:02 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -5,4 +5,16 @@ class Actor < ActiveRecord::Base
    name = "#{self.first_name} #{self.last_name}"
   end
 
+
+   def list_roles
+    lists = []
+    character_array = self.characters.collect { |character|character.name}
+    show_array = self.shows.collect { |show| show.name}
+    list = character_array.concat show_array 
+    lists << list.join(" - ")
+
+   # binding.pry
+  end
+
+
 end
